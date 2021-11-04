99cb18ddb99cd0aa9f1fa330c588f1d27a27526b
JH Web Design <jthogg84@gmail.com>
2021-11-04 13:02:52 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector('.nav');
+let btn = document.querySelector('.btn');
+let cls = document.querySelector('.cls');
+    btn.addEventListener('click', () =>{
+        nav.classList.toggle('open');
+    })
+
+    cls.addEventListener('click', () =>{
+        nav.classList.toggle('open');
+    })
